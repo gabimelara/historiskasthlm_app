5d5284ebae9d8465a4afe99b27cbe05df41f4cde
@LdwgP <ludwigpersson@hotmail.com>
2021-04-29 13:22:40 +0200

    Google map update HEJ

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:historiskasthlm_app/favoritesScreen.dart';
-import 'package:historiskasthlm_app/mapScreen.dart';
 import 'package:historiskasthlm_app/notificationScreen.dart';
 import 'package:historiskasthlm_app/searchScreen.dart';
-
 import 'overview.dart';
+import 'package:historiskasthlm_app/mapScreen.dart';
 //import 'package:flutter/services.dart';
 void main() {
   runApp(MyApp());
@@ -17,38 +17,66 @@ class MyApp extends StatelessWidget {
   //WIDGET ÄR APPENS ROT.
   @override
   Widget build(BuildContext context) {
-  return MaterialApp(
+    return MaterialApp(
 
-  theme: ThemeData(
-  // APPENS THEME
+      theme: ThemeData(
+        // APPENS THEME
 
-  textTheme: TextTheme(
-  headline1: TextStyle(
-  fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white), //RUBRIKEN
-  bodyText1: TextStyle(
-  fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold), // NÄSTA KNAPP
-  bodyText2: TextStyle(
-  fontSize: 20.0, height: 1.3, color: Colors.white, fontWeight: FontWeight.w200), //SKIPP KANPP
-  button: TextStyle(
-  fontSize: 20, color: Colors.pink, fontWeight: FontWeight.w700), //NU KÖR VI KNAPP
-  ),
-  // This makes the visual density adapt to the platform that you run
-  // the app on. For desktop platforms, the controls will be smaller and
-  // closer together (more dense) than on mobile platforms.
-  visualDensity: VisualDensity.adaptivePlatformDensity,
-  ),
-  home: Scaffold(body:HomePage()),
-  );
+        textTheme: TextTheme(
+          headline1: TextStyle(
+              fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.white), //RUBRIKEN
+          bodyText1: TextStyle(
+              fontSize: 20.0, color: Colors.white, fontWeight: FontWeight.bold), // NÄSTA KNAPP
+          bodyText2: TextStyle(
+              fontSize: 20.0, height: 1.3, color: Colors.white, fontWeight: FontWeight.w200), //SKIPP KANPP
+          button: TextStyle(
+              fontSize: 20, color: Colors.pink, fontWeight: FontWeight.w700), //NU KÖR VI KNAPP
+        ),
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(body:HomePage()),
+    );
 
   }
 
-  //@override
-  //Widget build(BuildContext context) {
-    //return MaterialApp(
-    //  home: StartScreen(),
-      // theme: ThemeData(
-    //);
+//@override
+//Widget build(BuildContext context) {
+//return MaterialApp(
+//  home: StartScreen(),
+// theme: ThemeData(
+//);
+}
+class GoogleScreen extends StatefulWidget {
+  @override
+  _GoogleScreenState createState() => _GoogleScreenState();
+
+}
+class _GoogleScreenState extends State<GoogleScreen> {
+  GoogleMapController mapController;
+  final LatLng _center = const LatLng(59.324554, 18.070520);
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: AppBar(
+        title: Center(child:Text('Historiska Stockholm', style: TextStyle(color:Colors.grey[900]))),
+        backgroundColor: Colors.orange[50],),
+
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 11.0,
+        ),
+      ),
+    );
+  }
+}
 
 
 class MyNavigationBar extends StatefulWidget {
@@ -59,7 +87,7 @@ class MyNavigationBar extends StatefulWidget {
 class _MyNavigationBarState extends State<MyNavigationBar> {
   int _currentIndex = 0; //// skrivs för att ikonen man klickar ska bli större
   final List<Widget> _screens=[
-    MapScreen(),
+    GoogleScreen(),
     SearchScreen(),
     FavoriteScreen(),
     NotificationScreen(),
@@ -107,19 +135,5 @@ class _MyNavigationBarState extends State<MyNavigationBar> {
     });
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
